edf77a6f28079a95e46db2bffd8f8ca26c8999e4
Rodrigo Silva <raswolf23@gmail.com>
2022-01-04 20:46:55 -0300

    Deck's cutter done

--- js/main.js
+++ js/main.js
@@ -1,67 +1,67 @@
 const cards = [
     {
-        tipo: "Agente",
-        forca: 1,
-        qtd: 2
+        name: "Agente",
+        str: 1,
+        qty: 2
     },
     {
-        tipo: "Soldado",
-        forca: 2,
-        qtd: 8
+        name: "Soldado",
+        str: 2,
+        qty: 8
     },
     {
-        tipo: "Cabo",
-        forca: 3,
-        qtd: 7
+        name: "Cabo",
+        str: 3,
+        qty: 7
     },
     {
-        tipo: "Sargento",
-        forca: 4,
-        qtd: 5
+        name: "Sargento",
+        str: 4,
+        qty: 5
     },
     {
-        tipo: "Subtenente",
-        forca: 5,
-        qtd: 5
+        name: "Subtenente",
+        str: 5,
+        qty: 5
     },
     {
-        tipo: "Tenente",
-        forca: 6,
-        qtd: 5
+        name: "Tenente",
+        str: 6,
+        qty: 5
     },
     {
-        tipo: "Capitão",
-        forca: 7,
-        qtd: 3
+        name: "Capitão",
+        str: 7,
+        qty: 3
     },
     {
-        tipo: "Major",
-        forca: 8,
-        qtd: 2
+        name: "Major",
+        str: 8,
+        qty: 2
     },
     {
-        tipo: "Coronel",
-        forca: 9,
-        qtd: 1
+        name: "Coronel",
+        str: 9,
+        qty: 1
     },
     {
-        tipo: "General",
-        forca: 10,
-        qtd: 1
+        name: "General",
+        str: 10,
+        qty: 1
     },
     {
-        tipo: "Mina",
-        forca: Infinity,
-        qtd: 6
+        name: "Mina",
+        str: Infinity,
+        qty: 6
     }
-]
+];
 
-function deckMount(){
+function deckMount(deck1, deck2){
     for(var i=0 ; i<11 ; i++){
-        let count = cards[i].qtd;
+        let count = cards[i].qty;
         for(let n=0 ; n<count ; n++){
-            deck1.push(cards[i].tipo);
-            deck2.push(cards[i].tipo);
+            deck1.push({name: cards[i].name, str: cards[i].str});
+            deck2.push({name: cards[i].name, str: cards[i].str});
         }        
     }
 }
@@ -85,10 +85,27 @@ function shuffle(deck) {
 }
 
 function cutDeck(deck){
-    
+    deck = [deck.slice(0,15), deck.slice(15,30), deck.slice(30,45)];
+    return deck;
 }
 
-let deck1 = [];
-let deck2 = [];
+let deckPlayer = [];
+let deckMachine = [];
+let deckPlayerWinners = [];
+let deckMachineWinners = [];
 
-deckMount();
+
+deckMount(deckPlayer, deckMachine);
+shuffle(deckPlayer);
+shuffle(deckMachine);
+deckPlayer = cutDeck(deckPlayer);
+deckMachine = cutDeck(deckMachine);
+
+let playerSeletedDeck = deckPlayer[0];
+let machineSeletedDeck =  deckMachine[Math.floor(Math.random() * 3)];
+
+function combat(deck1, deck2){
+    while(deck1.length>0 && deck2.length>0){
+        
+    }
+}
